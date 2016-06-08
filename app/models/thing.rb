d30da0f04cfea912ca29edfdbc9c7911714bcc96
Sam Blackman <sblackman@gmail.com>
2016-06-08 19:18:18 -0400

    write next method on thing model

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,3 +1,5 @@
 class Thing < ActiveRecord::Base
-  
+  def next(num_to_take)
+    self.class.where("id > ?", id).limit(num_to_take)
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  
+});
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+document.addEventListener('scroll', function (event) {
+  if (document.body.scrollHeight == 
+    document.body.scrollTop +        
+    window.innerHeight) {
+    alert("Bottom!");
+  }
 });
